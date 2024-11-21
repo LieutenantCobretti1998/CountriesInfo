e60d65d87ab4ac092f941bb6bd49929b6efe6028
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-11-21 21:01:08 +0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: [
-    "./src/api/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        "grey-main": "#eaeaea",
-        "grey-dark": "#c9c9c9",
-        "dark-bg": "rgba(30,30,30,0.75)",
-        "dark-bg-2": "rgba(0,0,0,0.75)",
-        "white": "#fff",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      fontFamily: {
-        nunito: ['"Nunito Sans"', 'ui-sans-serif', 'system-ui']
-      },
-      keyframes: {
-        "spin": {
-          "0%": {
-            transform: "rotate(0deg)"
-          },
-          "100%": {
-            transform: "rotate(360deg)"
-          }
-        }
-      },
-      animation: {
-        "spin": "spin 1s infinite linear",
-      }
     },
   },
   plugins: [],
